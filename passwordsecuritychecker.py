655954abb90bc912e2229528655d8ce0dd901894
Jayxvn <jaydn.westlake@gmail.com>
2026-05-30 12:36:29 -0400

    Add files via upload

---
+++ passwordsecuritychecker.py
@@ -0,0 +1,51 @@
+# Awesome password security checker man
+
+import string
+
+# Start loop
+answer = "y"
+while(answer != "n"):
+
+# Declaring variables
+    points = 0
+    password = str(input("Please input your password: "))
+
+    # Check password length
+    if len(password) < 8:
+        print("Your password isn't even long enough gang... ")
+    elif len(password) >= 15:
+        points += 2
+    elif len(password) >= 12:
+        points += 1
+
+    # Check any uppercases
+    if any(char.isupper() for char in password):
+        points += 1
+
+    # Check any lowercases
+    if any(char.islower() for char in password):
+        points += 1
+
+    # Check any digits
+    if any(char.isdigit() for char in password):
+        points += 1
+
+    # Check any symbols
+    if any(char in string.punctuation for char in password):
+        points += 1
+
+    # Judge based on points
+    if points <= 2:
+        print("You have a weak password.")
+    elif points <= 4:
+        print("You have a decent password.")
+    else:
+        print("You have a strong password!")
+
+    # Back to top of loop
+    while True:
+        answer = str(input("Would you like to try another password (y/n)? "))
+        if answer != "y" and answer != "n":
+            print("What?")
+        else:
+            break
